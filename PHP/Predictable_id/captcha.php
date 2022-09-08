34995031079de88c09d88fc329142a2e5f670fe1
CodiTrust <89127943+coditrust@users.noreply.github.com>
2022-09-08 16:08:27 +0200

    Update captcha.php

--- PHP/Predictable_id/captcha.php
+++ PHP/Predictable_id/captcha.php
@@ -1,17 +1,25 @@
 <?php
-session_start();
-$captcha_num = rand(1000, 9999);
-$_SESSION['code'] = $captcha_num;
 
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+        $captcha_num = rand(100, 999);
+        $_SESSION['code'] = $captcha_num;
+}
+
+$font_size = 30;
 $img_width = 120;
 $img_height = 30;
-
+ 
 header('Content-type: image/jpeg');
-
+ 
 $image = imagecreate($img_width, $img_height);
 imagecolorallocate($image, 255, 255, 255);
+ 
 $text_color = imagecolorallocate($image, 0, 0, 0);
 
-imagestring($image, 5, 5, 5, $captcha_num, $text_color);
+imagestring($image, 5, 5, 5, $_SESSION['code'], $text_color);
+
 imagejpeg($image);
 ?>
