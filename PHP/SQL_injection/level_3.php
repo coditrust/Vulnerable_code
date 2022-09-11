f7aebb66a0159b83a1bb38dcab1c577133a37ef9
CodiTrust <89127943+coditrust@users.noreply.github.com>
2022-09-11 19:31:57 +0200

    Create level_3.php
    Try to find how to extract information

---
+++ PHP/SQL_injection/level_3.php
@@ -0,0 +1,42 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+//include('settings.php'); // define $USER and $PASS for DB here
+
+function deleteBadChars($word)
+{
+      $word = str_replace(' or ', '', $word);
+      $word = str_replace(' OR ', '', $word);
+      $word = str_replace('-', '', $word);
+      $word = str_replace('\"', '', $word);
+      return $word;
+}
+
+$mysqli = new mysqli('localhost', $USER, $PASS, 'myapp');
+
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+$login = deleteBadChars($_GET['login']);
+$password = deleteBadChars($_GET['password']);
+
+$req = "SELECT * FROM Persons WHERE login='".$login."' AND password='".$password."'";
+$result = $mysqli->query($req);
+
+if ($result == false ) {
+     echo("Error description: " . $mysqli->error);
+     exit();
+}
+
+if($result->num_rows >= 1)
+{
+      echo "Good job, you are authenticated!!";
+      $result -> free_result();
+}else{
+   echo $mysqli->error;
+}
+
+$mysqli -> close();
+?>
